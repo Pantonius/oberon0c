73caeca3103a1d9f51f8ba05fc5e1f2d156217d8
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-11-22 13:30:22 +0100

    WIP: afl++ build setup

---
+++ afl.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  makeWrapper,
+}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "oberon0c-fuzz";
+  version = "0.1.0";
+
+  src = ./.;
+
+  buildInputs =
+    (import ./dependencies.nix {
+      inherit pkgs;
+    })
+    ++ [
+      makeWrapper
+      pkgs.aflplusplus
+    ];
+
+  buildPhase = ''
+    mkdir build
+    cd build
+
+    cmake -DCMAKE_C_COMPILER=afl-cc -DCMAKE_CXX_COMPILER=afl-c++ ..
+    cmake --build . --parallel $NIX_BUILD_CORES
+  '';
+
+  checkPhase = ''
+
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv oberon0c $out/bin
+
+    runHook postInstall
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Basic setup for the Oberon-0 Compiler project of the course \"Compiler Construction\".";
+    homepage = "https://gitlab.inf.uni-konstanz.de/anton.pogrebnjak/${pname}";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
