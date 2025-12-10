98f040db1eca89e760523388775ea626c709a0fe
Anton Pogrebnjak <contact@pantonius.dev>
2025-12-10 08:29:18 +0100

    Condensed Declaration Nodes into DeclSeqNode

--- parser/ast/DeclarationSequenceNode.h
+++ parser/ast/DeclarationSequenceNode.h
@@ -1,16 +1,70 @@
 #ifndef OBERON0C_DECLARATIONSEQUENCENODE_H
 #define OBERON0C_DECLARATIONSEQUENCENODE_H
 
-#include "ConstDeclarationNode.h"
 #include "Node.h"
-#include "ProcedureDeclarationNode.h"
-#include "TypeDeclarationNode.h"
-#include "VarDeclarationNode.h"
+#include "ProcedureBodyNode.h"
+#include "ProcedureHeadingNode.h"
+#include "TypeNode.h"
 #include <memory>
 #include <vector>
 
 using std::unique_ptr;
 using std::vector;
+
+class ConstDeclarationNode final : public Node {
+public:
+  ConstDeclarationNode(const FilePos &pos)
+      : Node(NodeType::const_declaration, pos) {}
+  ~ConstDeclarationNode() override = default;
+
+  void accept(NodeVisitor &visitor) override {};
+  void print(std::ostream &stream) const override {};
+
+  string ident;
+  unique_ptr<ExpressionNode> expression;
+};
+
+class ProcedureDeclarationNode final : public Node {
+public:
+  ProcedureDeclarationNode(const FilePos &pos)
+      : Node(NodeType::procedure_declaration, pos) {}
+  ~ProcedureDeclarationNode() override = default;
+
+  void accept(NodeVisitor &visitor) override {};
+  void print(std::ostream &stream) const override {};
+
+  unique_ptr<ProcedureHeadingNode> heading;
+  unique_ptr<ProcedureBodyNode> body;
+  string ident;
+};
+
+class TypeDeclarationNode final : public Node {
+public:
+  TypeDeclarationNode(const FilePos &pos)
+      : Node(NodeType::type_declaration, pos) {}
+  ~TypeDeclarationNode() override = default;
+
+  void accept(NodeVisitor &visitor) override {};
+  void print(std::ostream &stream) const override {};
+
+  string ident;
+  unique_ptr<TypeNode> type;
+};
+
+class VarDeclarationNode final : public Node {
+public:
+  VarDeclarationNode(const FilePos &pos)
+      : Node(NodeType::var_declaration, pos) {}
+  ~VarDeclarationNode() override = default;
+
+  void accept(NodeVisitor &visitor) override {};
+  void print(std::ostream &stream) const override {};
+
+  string ident;
+  bool exported;
+  unique_ptr<ExpressionNode> expression;
+  string type;
+};
 
 class DeclarationSequenceNode final : public Node {
 public:
