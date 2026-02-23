666d748bd13d41df745603719061720698a25cd2
Anton Pogrebnjak <contact@pantonius.dev>
2026-02-23 12:11:25 +0100

    Added test for record type

--- tests/semantic_checker/type.cpp
+++ tests/semantic_checker/type.cpp
@@ -54,3 +54,40 @@ TEST_CASE("Sema Array Type", "[sema][type][array]") {
         NonConstException);
   }
 }
+
+TEST_CASE("Sema Record Type", "[sema][type][record]") {
+  Logger logger;
+  SemanticChecker sema(logger);
+
+  auto module_ident = make_unique<IdentNode>(EMPTY_POS, "Module");
+
+  sema.onModuleStart(EMPTY_POS, std::move(module_ident));
+
+  SECTION("Record fields") {
+    vector<pair<vector<unique_ptr<IdentNode>>, TypeNode *>> fields;
+
+    vector<unique_ptr<IdentNode>> var_idents_int;
+    var_idents_int.push_back(std::make_unique<IdentNode>(EMPTY_POS, "field1"));
+    var_idents_int.push_back(std::make_unique<IdentNode>(EMPTY_POS, "field2"));
+
+    fields.emplace_back(std::move(var_idents_int), ASTContext::INTEGER.get());
+
+    vector<unique_ptr<IdentNode>> var_idents_bool;
+    var_idents_bool.push_back(std::make_unique<IdentNode>(EMPTY_POS, "field3"));
+    var_idents_bool.push_back(std::make_unique<IdentNode>(EMPTY_POS, "field4"));
+
+    fields.emplace_back(std::move(var_idents_bool), ASTContext::BOOLEAN.get());
+
+    auto record = sema.onRecordType(EMPTY_POS, std::move(fields));
+
+    REQUIRE(record->field_lists[0]->ident->value == "field1");
+    REQUIRE(record->field_lists[0]->type == ASTContext::INTEGER.get());
+    REQUIRE(record->field_lists[1]->ident->value == "field2");
+    REQUIRE(record->field_lists[1]->type == ASTContext::INTEGER.get());
+
+    REQUIRE(record->field_lists[2]->ident->value == "field3");
+    REQUIRE(record->field_lists[2]->type == ASTContext::BOOLEAN.get());
+    REQUIRE(record->field_lists[3]->ident->value == "field4");
+    REQUIRE(record->field_lists[3]->type == ASTContext::BOOLEAN.get());
+  }
+}
