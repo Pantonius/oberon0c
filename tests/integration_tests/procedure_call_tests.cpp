3a6a5050806c3a77422d3da39d2e34e78adadee4
Anton Pogrebnjak <contact@pantonius.dev>
2026-02-24 23:08:06 +0100

    Integration test for proc without args

--- tests/integration_tests/procedure_call_tests.cpp
+++ tests/integration_tests/procedure_call_tests.cpp
@@ -64,4 +64,40 @@ TEST_CASE("Test ProcedureCalls"
     REQUIRE(*i == 2);
     REQUIRE(*j == 1);
   }
+
+  SECTION("File EmptyProcedureCall.Mod") {
+    Logger logger(LogLevel::DEBUG, std::cout, std::cerr);
+    const path path = "./integration_tests/resources/EmptyProcedureCall.Mod";
+    Scanner scanner(path, logger);
+
+    Parser parser(scanner, logger);
+
+    auto ast = parser.parse();
+
+    auto module = setup_llvm_module();
+
+    CodeGenBuilder builder(logger, *module);
+    builder.build(*ast);
+
+    llvm::Function *main = module->getFunction("main");
+
+    if (!main) {
+      FAIL("No main function found in module");
+    }
+
+    auto var_i = module->getGlobalVariable("i", true);
+
+    if (!var_i) {
+      FAIL("No global variable i");
+    }
+
+    auto exec_engine = create_jit(std::move(module));
+
+    exec_engine->runFunction(main, {});
+
+    auto i_addr = exec_engine->getGlobalValueAddress("i");
+    auto i = (int *)i_addr;
+
+    REQUIRE(*i == 16);
+  }
 }
