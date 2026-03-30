56edd567dc4aa4e54a4bae6d647e34aa672f1d54
Anton Pogrebnjak <contact@pantonius.dev>
2026-03-30 23:46:15 +0200

    Latest version

--- codegen/CodeGen.cpp
+++ codegen/CodeGen.cpp
@@ -769,9 +769,9 @@ void CodeGenBuilder::visit(CaseStatementNode &case_stmt) {
   const auto right_type = case_stmt.value->type;
   const auto right_value = value_;
 
-  if (right_type == ASTContext::INTEGER || right_type == ASTContext::BOOLEAN) {
-    u_int first_reachable_case = case_stmt.reachable_cases.at(0);
+  u_int first_reachable_case = case_stmt.reachable_cases.at(0);
 
+  if (right_type == ASTContext::INTEGER || right_type == ASTContext::BOOLEAN) {
     if (cases->at(first_reachable_case).first->getNodeType() ==
         NodeType::ident_pattern) {
       // just var declaration and assignment
@@ -867,8 +867,144 @@ void CodeGenBuilder::visit(CaseStatementNode &case_stmt) {
       builder_->SetInsertPoint(tailBlock);
     }
   } else if (case_stmt.value->getNodeType() == NodeType::sum_type) {
-    logger_.error(case_stmt.value->pos(),
-                  "Can't generate code for sum type values.");
+    auto sum_type = dynamic_cast<SumTypeNode *>(case_stmt.value->type);
+
+    if (cases->at(first_reachable_case).first->getNodeType() ==
+        NodeType::ident_pattern) {
+      // just var declaration and assignment
+      cases->at(first_reachable_case)
+          .first->accept(*this); // visit VariantPatternNode
+      auto ident_pattern = dynamic_cast<const IdentPatternNode *>(
+          cases->at(first_reachable_case).first.get());
+
+      llvm::AllocaInst *base_ptr;
+      try {
+        base_ptr = static_cast<llvm::AllocaInst *>(
+            values_.at(ident_pattern->var.get()));
+      } catch (std::out_of_range &e) {
+        logger_.debug("Unknown variable: " +
+                      to_string(*ident_pattern->var->ident));
+      }
+
+      get_elem_ptr(ident_pattern->var.get(), base_ptr, {});
+
+      auto left_type = ident_pattern->type;
+      auto left_value = value_;
+
+      auto llvm_ltype = getLLVMType(left_type);
+      auto llvm_rtype = getLLVMType(right_type);
+
+      auto lsize = module_.getDataLayout().getTypeAllocSize(llvm_ltype);
+      auto rsize = module_.getDataLayout().getTypeAllocSize(llvm_rtype);
+
+      if (lsize != rsize) {
+        logger_.debug("Assignment sizes differ");
+        exit(EXIT_FAILURE);
+      }
+
+      value_ = builder_->CreateMemCpy(left_value, {}, right_value, {}, lsize);
+    } else {
+      auto tailBlock = llvm::BasicBlock::Create(builder_->getContext(),
+                                                "ifTail", currentFunc);
+      auto trueBlock = llvm::BasicBlock::Create(builder_->getContext(),
+                                                "ifTrue", currentFunc);
+      auto falseBlock = llvm::BasicBlock::Create(builder_->getContext(),
+                                                 "ifFalse", currentFunc);
+
+      std::unordered_map<int, vector<u_int>>
+          variant_map; // variant_tag -> case_indicies
+
+      for (size_t i = 1; i < case_stmt.reachable_cases.size(); i++) {
+        if (case_stmt.get_cases()->at(i).first->getNodeType() ==
+            NodeType::ident_pattern) {
+          continue;
+        }
+
+        auto variant_pattern = dynamic_cast<const VariantPatternNode *>(
+            case_stmt.get_cases()->at(i).first.get());
+
+        auto case_index = case_stmt.reachable_cases.at(i);
+        auto variant_index =
+            sum_type->find_variant_index(*variant_pattern->variant->ident);
+
+        if (!variant_map.contains(variant_index)) {
+          variant_map[variant_index] = {};
+        }
+
+        variant_map[variant_index].push_back(case_index);
+      }
+
+      auto first_variant_index = variant_map.begin()->first;
+
+      const auto if_right_value = builder_->CreateConstGEP2_32(
+          getLLVMType(sum_type), right_value, 0, 0);
+
+      const auto if_left_value =
+          llvm::ConstantInt::get(builder_->getInt32Ty(), first_variant_index);
+
+      // match variant tag
+      auto if_condition = builder_->CreateICmpEQ(if_left_value, if_right_value);
+      builder_->CreateCondBr(if_condition, trueBlock, falseBlock);
+
+      builder_->SetInsertPoint(trueBlock);
+      // TODO
+
+      builder_->CreateBr(tailBlock);
+
+      builder_->SetInsertPoint(falseBlock);
+
+      for (size_t i = 1; i < case_stmt.reachable_cases.size(); i++) {
+        auto case_index = case_stmt.reachable_cases.at(i);
+        if (cases->at(case_index).first->getNodeType() ==
+            NodeType::ident_pattern) {
+          // "ELSE"
+          cases->at(case_index).first->accept(*this); // visit IdentPatternNode
+          auto ident_pattern = dynamic_cast<const IdentPatternNode *>(
+              cases->at(case_index).first.get());
+
+          llvm::AllocaInst *base_ptr;
+          try {
+            base_ptr = static_cast<llvm::AllocaInst *>(
+                values_.at(ident_pattern->var.get()));
+          } catch (std::out_of_range &e) {
+            logger_.debug("Unknown variable: " +
+                          to_string(*ident_pattern->var->ident));
+          }
+
+          get_elem_ptr(ident_pattern->var.get(), base_ptr, {});
+
+          const auto left_value = value_;
+          value_ = builder_->CreateStore(right_value, left_value);
+
+          cases->at(case_index).second->accept(*this);
+
+        } else if (cases->at(case_index).first->getNodeType() ==
+                   NodeType::literal_pattern) {
+          // "ELSIF"
+          cases->at(case_index).first->accept(*this);
+          const auto elsif_left_value = value_;
+
+          auto elsif_condition =
+              builder_->CreateICmpEQ(elsif_left_value, right_value);
+
+          auto elsIfTrueBlock = llvm::BasicBlock::Create(builder_->getContext(),
+                                                         "ifTrue", currentFunc);
+          auto elsIfFalseBlock = llvm::BasicBlock::Create(
+              builder_->getContext(), "ifFalse", currentFunc);
+
+          builder_->CreateCondBr(elsif_condition, elsIfTrueBlock,
+                                 elsIfFalseBlock);
+
+          builder_->SetInsertPoint(elsIfTrueBlock);
+          cases->at(case_index).second->accept(*this);
+          builder_->CreateBr(tailBlock);
+
+          builder_->SetInsertPoint(elsIfFalseBlock);
+        }
+      }
+      builder_->CreateBr(tailBlock);
+      builder_->SetInsertPoint(tailBlock);
+    }
   } else {
     logger_.error(case_stmt.value->pos(), "UNEXPECTED VALUE TYPE");
   }
